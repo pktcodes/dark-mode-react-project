586c719959a8ba04480956392934540999335db6
Praveen Kumar <praveenkumarthipparam@gmail.com>
2022-06-26 18:48:07 -0500

    Local Storage

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import React, { useState, useEffect } from "react";
 import data from "./data";
 import Article from "./Article";
 
+const getStorageTheme = () => {
+  let theme = "light-theme";
+  if (localStorage.getItem("theme")) {
+    theme = localStorage.getItem("theme");
+  }
+  return theme;
+};
+
 function App() {
-  const [theme, setTheme] = useState("light-theme");
+  const [theme, setTheme] = useState(getStorageTheme());
 
   const toggleTheme = () => {
     if (theme === "light-theme") {
@@ -16,6 +24,7 @@ function App() {
   useEffect(() => {
     // console.log(document.documentElement);
     document.documentElement.className = theme;
+    localStorage.setItem("theme", theme);
   }, [theme]);
 
   return (
