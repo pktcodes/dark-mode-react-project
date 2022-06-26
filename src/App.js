5da695c51d7f5d3261e418f77d9d58c0ee9bcec4
Praveen Kumar <praveenkumarthipparam@gmail.com>
2022-06-26 15:56:30 -0500

    Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React, { useState, useEffect } from 'react'
-import data from './data'
-import Article from './Article'
+import React, { useState, useEffect } from "react";
+import data from "./data";
+import Article from "./Article";
 
 function App() {
-  return <h2>dark mode starter</h2>
+  return (
+    <main>
+      <nav>
+        <div className="nav-center">
+          <h1>overreacted</h1>
+          <button className="btn">toggle</button>
+        </div>
+        <section className="articles">
+          {data.map((item) => {
+            return <Article key={item.id} {...item}></Article>;
+          })}
+        </section>
+      </nav>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- src/Article.js
+++ src/Article.js
@@ -1,7 +1,17 @@
-import React from 'react'
-import moment from 'moment'
-const Article = () => {
-  return <h2>article component</h2>
-}
+import React from "react";
+import moment from "moment";
+const Article = ({ title, date, length, snippet }) => {
+  console.log(date);
+  return (
+    <article className="post">
+      <h2>{title}</h2>
+      <div className="post-info">
+        <span>date</span>
+        <span>{length} min read</span>
+      </div>
+      <p>{snippet}</p>
+    </article>
+  );
+};
 
-export default Article
+export default Article;
