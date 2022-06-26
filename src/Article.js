0f68d47256c6ee954b2d4eebc93d9a9902b0c161
Praveen Kumar <praveenkumarthipparam@gmail.com>
2022-06-26 16:32:41 -0500

    Moment.js

--- src/Article.js
+++ src/Article.js
@@ -1,12 +1,11 @@
 import React from "react";
 import moment from "moment";
 const Article = ({ title, date, length, snippet }) => {
-  console.log(date);
   return (
     <article className="post">
       <h2>{title}</h2>
       <div className="post-info">
-        <span>date</span>
+        <span>{moment(date).format("dddd Do, YYYY")}</span>
         <span>{length} min read</span>
       </div>
       <p>{snippet}</p>
